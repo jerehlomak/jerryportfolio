99b290709c3b6f3e661efd827f98e658fe277bc0
jerehlomak <jerehlomak@gmail.com>
2024-04-30 12:48:21 +0100

    cc

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
         {/* <img src={images.logo} alt="logo" /> */}
-        Jereh
+        <a href="#home">Jereh</a>
       </div>
       <ul className='app__navbar-links'>
-        {['home', 'about', 'contact', 'work', 'skills', 'testimonials'].map((item, index) => (
+        {['home', 'about', 'contact', 'work', 'skills', 'testimonial'].map((item, index) => (
           <li className='app__flex p-text' key={index}>
             <div />
             <a href={`#${item}`}>{item}</a> 
@@ -34,7 +34,7 @@ const Navbar = () => {
               >
                 <HiX onClick={() => setToggle(false)} />
                 <ul>
-                  {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+                  {['home', 'about', 'contact', 'work', 'skills', 'testimonial'].map((item) => (
                     <li key={item}>
                       <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                     </li>

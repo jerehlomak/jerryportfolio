951be116ceaae9ee25f39f8b435a27312e9dc116
jerehlomak <jerehlomak@gmail.com>
2024-04-30 12:21:55 +0100

    cccc

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             toggle && (
               <motion.div
                 whileInView={{ x: [300, 0] }}
-                transition={{ duration: 0.25, ease: 'easeOut' }}
+                transition={{ duration: 0.1, ease: 'linear' }}
               >
                 <HiX onClick={() => setToggle(false)} />
                 <ul>
